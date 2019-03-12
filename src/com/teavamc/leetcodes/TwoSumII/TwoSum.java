7f6511ba82b98ed330deee328ee3f148da8f2f09
teavamc <teavamc@163.com>
2019-03-12 22:25:51 +0800

    pass leetcode 167

---
+++ src/com/teavamc/leetcodes/TwoSumII/TwoSum.java
@@ -0,0 +1,41 @@
+package com.teavamc.leetcodes.TwoSumII;
+
+/**
+ * @author 张超 teavamc
+ * @Description:
+ *
+ * 给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+ *
+ * 函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+ *
+ * 说明:
+ *
+ * 返回的下标值（index1 和 index2）不是从零开始的。
+ * 你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+ * 示例:
+ *
+ * 输入: numbers = [2, 7, 11, 15], target = 9
+ * 输出: [1,2]
+ * 解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+ *
+ * @ClassName TwoSum
+ * @date 2019/3/12 21:56
+ **/
+public class TwoSum {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        int aim;
+        while (left < right) {
+            aim = numbers[left] + numbers[right];
+            if (target == aim) {
+                return new int[]{left + 1, right + 1};
+            } else if (target < aim) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return null;
+    }
+}
