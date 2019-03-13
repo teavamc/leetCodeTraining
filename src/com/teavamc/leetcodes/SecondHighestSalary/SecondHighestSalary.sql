baecd2e51b0440ddda3027fe51390913148c00fd
teavamc <teavamc@163.com>
2019-03-13 12:51:26 +0800

    pass leetcode 176

---
+++ src/com/teavamc/leetcodes/SecondHighestSalary/SecondHighestSalary.sql
@@ -0,0 +1,10 @@
+select (
+         select distinct
+           Salary
+         from
+           Employee
+         order by
+           Salary
+           desc
+         limit 1,1
+       )as SecondHighestSalary
